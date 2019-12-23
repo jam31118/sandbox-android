3dba43a875fc94754cd13733b7c7d1d3072d7ccb
jam31118 <jam45@naver.com>
2019-12-23 16:59:14 +0900

    Add the toolbar into the second activity
    - a separate XML layout file for the toolbar is generated

--- app/src/main/java/com/example/multiactivities/MainActivity.java
+++ app/src/main/java/com/example/multiactivities/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.multiactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
-//import android.support.v7.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
--- app/src/main/java/com/example/multiactivities/SecondActivity.java
+++ app/src/main/java/com/example/multiactivities/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.multiactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
